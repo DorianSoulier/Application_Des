37d45bdbf1fc2f7f7dec47f07adb04c32ffc5873
dorian <doriansoulier.pro@gmail.com>
2021-10-20 11:26:46 +0200

    choix du nombre de dés fonctionnel

--- app/src/main/java/com/btsinfo/evaluation_des/MainActivity.java
+++ app/src/main/java/com/btsinfo/evaluation_des/MainActivity.java
@@ -8,15 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup rgNbFaces;
+    private RadioGroup rgNbDes;
     private RadioButton rBtFaces;
+    private RadioButton rBtDes;
     private Button btLancer;
-    private TextView tvResultat;
     private Double resultat = 0.0;
 
 
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rgNbFaces = (RadioGroup)  findViewById(R.id.rgNbFaces);
+        rgNbDes = (RadioGroup) findViewById(R.id.rgNbDes);
         btLancer = (Button) findViewById(R.id.btLancer);
-
 
         btLancer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,18 +37,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
     public void toResult() {
-        int radioId = rgNbFaces.getCheckedRadioButtonId();
-        rBtFaces = (RadioButton) findViewById(radioId);
+        int radioId1 = rgNbFaces.getCheckedRadioButtonId();
+        rBtFaces = (RadioButton) findViewById(radioId1);
+        int radioId2 = rgNbDes.getCheckedRadioButtonId();
+        rBtDes = (RadioButton) findViewById(radioId2);
         resultat = 0.0;
         Intent intent = new Intent(this, ResultActivity.class);
         intent.putExtra("resultat", resultat);
         intent.putExtra("nbFaces", rBtFaces.getText().toString());
+        intent.putExtra("nbDes", rBtDes.getText().toString());
         startActivity(intent);
     }
-
-
-
 }
--- app/src/main/java/com/btsinfo/evaluation_des/ResultActivity.java
+++ app/src/main/java/com/btsinfo/evaluation_des/ResultActivity.java
@@ -13,8 +13,10 @@ public class ResultActivity extends AppCompatActivity {
 
 
     private TextView tvResultat;
-    private String NbFaces;
+    private String nbFaces;
+    private String nbDes;
     private Double resultat;
+    private Double i = 0.0;
 
 
 
@@ -25,74 +27,352 @@ public class ResultActivity extends AppCompatActivity {
 
         tvResultat = (TextView) findViewById(R.id.tvResultat);
         Intent intent = getIntent();
-         resultat = intent.getDoubleExtra("resultat",0.0);
-         NbFaces = intent.getStringExtra("nbFaces");
+        resultat = intent.getDoubleExtra("resultat",0.0);
+        nbFaces = intent.getStringExtra("nbFaces");
+        nbDes = intent.getStringExtra("nbDes");
 
-         // Calcul
-        if (NbFaces.equalsIgnoreCase("4")) {
+        // Calcul
+        // Pour 1 dé
+        if (nbDes.equalsIgnoreCase("1")) {
+            if (nbFaces.equalsIgnoreCase("4")) {
+                while (i<1) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(4);
+                    int number = 1 + random.nextInt(4);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("6")) {
+                while (i<1) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(6);
+                    int number = 1 + random.nextInt(6);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("8")) {
+                while (i<1) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(8);
+                    int number = 1 + random.nextInt(8);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("10")) {
+                while (i<1) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(10);
+                    int number = 1 + random.nextInt(10);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("12")) {
+                while (i<1) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(12);
+                    int number = 1 + random.nextInt(12);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("20")) {
+                while (i<1) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(20);
+                    int number = 1 + random.nextInt(20);
+                    resultat += number;
+                }
+            }
+        }
+
+
+
+
+        // Pour 2 dés
+        if (nbDes.equalsIgnoreCase("2")) {
+            if (nbFaces.equalsIgnoreCase("4")) {
+                while (i<2) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(4);
+                    int number = 1 + random.nextInt(4);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("6")) {
+                while (i<2) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(6);
+                    int number = 1 + random.nextInt(6);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("8")) {
+                while (i<2) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(8);
+                    int number = 1 + random.nextInt(8);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("10")) {
+                while (i<2) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(10);
+                    int number = 1 + random.nextInt(10);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("12")) {
+                while (i<2) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(12);
+                    int number = 1 + random.nextInt(12);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("20")) {
+                while (i<2) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(20);
+                    int number = 1 + random.nextInt(20);
+                    resultat += number;
+                }
+            }
+        }
+
+        // Pour 3 dés
+        if (nbDes.equalsIgnoreCase("3")) {
+            if (nbFaces.equalsIgnoreCase("4")) {
+                while (i<3) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(4);
+                    int number = 1 + random.nextInt(4);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("6")) {
+                while (i<3) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(6);
+                    int number = 1 + random.nextInt(6);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("8")) {
+                while (i<3) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(8);
+                    int number = 1 + random.nextInt(8);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("10")) {
+                while (i<3) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(10);
+                    int number = 1 + random.nextInt(10);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("12")) {
+                while (i<3) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(12);
+                    int number = 1 + random.nextInt(12);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("20")) {
+                while (i<3) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(20);
+                    int number = 1 + random.nextInt(20);
+                    resultat += number;
+                }
+            }
+        }
+
+        // Pour 4 dés
+        if (nbDes.equalsIgnoreCase("4")) {
+            if (nbFaces.equalsIgnoreCase("4")) {
+                while (i<4) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(4);
+                    int number = 1 + random.nextInt(4);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("6")) {
+                while (i<4) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(6);
+                    int number = 1 + random.nextInt(6);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("8")) {
+                while (i<4) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(8);
+                    int number = 1 + random.nextInt(8);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("10")) {
+                while (i<4) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(10);
+                    int number = 1 + random.nextInt(10);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("12")) {
+                while (i<4) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(12);
+                    int number = 1 + random.nextInt(12);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("20")) {
+                while (i<4) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(20);
+                    int number = 1 + random.nextInt(20);
+                    resultat += number;
+                }
+            }
+        }
+        // Pour 5 dés
+        if (nbDes.equalsIgnoreCase("5")) {
+            if (nbFaces.equalsIgnoreCase("4")) {
+                while (i<5) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(4);
+                    int number = 1 + random.nextInt(4);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("6")) {
+                while (i<5) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(6);
+                    int number = 1 + random.nextInt(6);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("8")) {
+                while (i<5) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(8);
+                    int number = 1 + random.nextInt(8);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("10")) {
+                while (i<5) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(10);
+                    int number = 1 + random.nextInt(10);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("12")) {
+                while (i<5) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(12);
+                    int number = 1 + random.nextInt(12);
+                    resultat += number;
+                }
+            }
+            if (nbFaces.equalsIgnoreCase("20")) {
+                while (i<5) {
+                    i += 1.0;
+                    Random random = new Random();
+                    random.nextInt(20);
+                    int number = 1 + random.nextInt(20);
+                    resultat += number;
+                }
+            }
+        }
+
+
+        /*if (nbFaces.equalsIgnoreCase("4")) {
             Random random = new Random();
             random.nextInt(4);
             int number = 1 + random.nextInt(4);
 
             resultat += number;
         }
-        if (NbFaces.equalsIgnoreCase("6")) {
+        if (nbFaces.equalsIgnoreCase("6")) {
             Random random = new Random();
             random.nextInt(6);
             int number = 1 + random.nextInt(6);
 
             resultat += number;
         }
-        if (NbFaces.equalsIgnoreCase("8")) {
+        if (nbFaces.equalsIgnoreCase("8")) {
             Random random = new Random();
             random.nextInt(8);
             int number = 1 + random.nextInt(8);
 
             resultat += number;
         }
-        if (NbFaces.equalsIgnoreCase("10")) {
+        if (nbFaces.equalsIgnoreCase("10")) {
             Random random = new Random();
             random.nextInt(10);
             int number = 1 + random.nextInt(10);
 
             resultat += number;
         }
-        if (NbFaces.equalsIgnoreCase("12")) {
+        if (nbFaces.equalsIgnoreCase("12")) {
             Random random = new Random();
             random.nextInt(12);
             int number = 1 + random.nextInt(12);
 
             resultat += number;
         }
-        if (NbFaces.equalsIgnoreCase("20")) {
+        if (nbFaces.equalsIgnoreCase("20")) {
             Random random = new Random();
             random.nextInt(20);
             int number = 1 + random.nextInt(20);
 
             resultat += number;
         }
-
+        */
 
 
         // Resultat
         String monresultat = "Le lancé a donné : " + String.valueOf(resultat);
         tvResultat.setText(monresultat);
 
-
-
-
-
-
     }
 }
-
-            /*class GenerateurNb {
-                public void main(String[] args) {
-                    int min_val = 1;
-                    int max_val = 4;
-                    SecureRandom rand = new SecureRandom();
-                    rand.setSeed(new Date().getTime());
-                    int randomNum = rand.nextInt((max_val - min_val) + 1) + min_val;
-                    System.out.println(randomNum);
-                }
-            }*/
